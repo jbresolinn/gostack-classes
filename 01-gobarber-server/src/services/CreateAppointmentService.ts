dbe98a2db3f7303d23bc61366196d897a32f4c17
jbresolinn <juliabresolin2@gmail.com>
2020-09-12 17:09:04 -0300

    (01) perf: register data in database

--- 01-gobarber-server/src/services/CreateAppointmentService.ts
+++ 01-gobarber-server/src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from "date-fns";
+import { getCustomRepository } from "typeorm";
 import Appointment from "../models/Appointment";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
@@ -8,27 +9,26 @@ interface RequestDTO {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
+  public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentRepository;
-  }
-
-  public execute({ date, provider }: RequestDTO): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate
     );
 
     if (findAppointmentInSameDate) {
-      throw Error("❌ This appointment is already booked! ");
+      throw Error("This appointment is already booked! ");
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
+
     return appointment;
   }
 }
