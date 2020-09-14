1b27469f27e2787ceeaf3531f5f94eecbc71fbf8
jbresolinn <juliabresolin2@gmail.com>
2020-09-14 13:16:05 -0300

    (01) feat: updated user avatar

--- 01-gobarber-server/src/routes/users.routes.ts
+++ 01-gobarber-server/src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
+import uploadConfig from '../config/upload';
+import UpdatedUserAvatarService from '../services/UpdatedUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -15,5 +20,25 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    try {
+      const updatedAvatar = new UpdatedUserAvatarService();
+
+      const user = await updatedAvatar.execute({
+        user_id: req.user.id,
+        avatar_filename: req.file.filename,
+      });
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
---
+++ 01-gobarber-server/src/services/UpdatedUserAvatarService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import { resolve, join } from 'path';
+import fs from 'fs';
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface Request {
+  user_id: string;
+  avatar_filename: string;
+}
+
+class UpdatedUserAvatarService {
+  public async execute({ user_id, avatar_filename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar!');
+    }
+
+    // check if the user already has an avatar and if he already has, the system deletes the same
+
+    if (user.avatar) {
+      const userAvatarFilePath = join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatar_filename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdatedUserAvatarService;
