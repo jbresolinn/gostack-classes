3cc0f1349c41e429ec6e01b085fe39ac00f43537
jbresolinn <juliabresolin2@gmail.com>
2020-09-14 20:43:35 -0300

    (02) layout: dashboard layout

--- 02-first-project-react/src/pages/Dashboard/index.tsx
+++ 02-first-project-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,61 @@
 import React from 'react';
-import { Title } from './styles';
+
+import { FiChevronRight } from 'react-icons/fi';
+import logoImage from '../../assets/images/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no github!</Title>;
+  return (
+    <>
+      <img src={logoImage} alt="github explorer logo" />
+      <Title>Explore repositórios no github!</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://api.adorable.io/avatars/285/abott@adorable.png"
+            alt="user avatar"
+          />
+          <div>
+            <strong>jbresolinn/repo-teste</strong>
+            <p>Aqui vai a descrição bem bonitinha.</p>
+          </div>
+
+          <FiChevronRight size={24} />
+        </a>
+        <a href="/">
+          <img
+            src="https://api.adorable.io/avatars/285/abott@adorable.png"
+            alt="user avatar"
+          />
+          <div>
+            <strong>jbresolinn/repo-teste</strong>
+            <p>Aqui vai a descrição bem bonitinha.</p>
+          </div>
+
+          <FiChevronRight size={24} />
+        </a>
+        <a href="/">
+          <img
+            src="https://api.adorable.io/avatars/285/abott@adorable.png"
+            alt="user avatar"
+          />
+          <div>
+            <strong>jbresolinn/repo-teste</strong>
+            <p>Aqui vai a descrição bem bonitinha.</p>
+          </div>
+
+          <FiChevronRight size={24} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
