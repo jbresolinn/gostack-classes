27ed392779804b270226f03884c9feadee54f9f1
jbresolinn <juliabresolin2@gmail.com>
2020-09-16 16:26:37 -0300

    (03)layout: sign in layout

---
+++ 03-gobarber-web/src/pages/SignIn/styles.ts
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import SignInBackground from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  max-width: 768px;
+  align-items: center;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #F4EDE8;
+
+      &::placeholder {
+        color: #666360;
+      }
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #ff9000;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      width: 100%;
+      height: 56px;
+      color: #312e38;
+      font-weight: 600;
+      margin-top: 16px;
+      transition: background .3s;
+
+      &:hover {
+        background: ${shade(.2, '#ff9000')};
+      }
+    }
+
+      a {
+      color: #F4EDE8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color .2s;
+      &:hover {
+        color: ${shade(.2, '#F4EDE8')};
+      }
+    }
+  }
+
+  > a {
+    color: #ff9000;
+      display: flex;
+      align-items: center;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color .2s;
+
+      &:hover {
+        color: ${shade(.2, '#ff9000')};
+      }
+
+      svg {
+        margin-right: 16px;
+      }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${SignInBackground}) no-repeat center ;
+  background-size: cover;
+`;
